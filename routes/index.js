39dff8c8ec2c9170ffb45811df95d9d9ca994945
Yang Yang <yangyang@CDDEV01.cd.ad.availink.com>
2017-09-13 17:44:59 +0800

    add remote debug

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,18 @@ router.get('/', function(req, res, next) {
 
 var i = 0
 router.get('/wechat', function(req, res, next) {
-  api.sendText('o3NPW0r_fi2BL8-zYrJuryidCuCY', 'Hello ' + ++ i, (err, r) => {
+  api.sendText('oByqawkBy0a0j4COOy04IcyXRP2Y', 'Hello ' + ++ i, (err, r) => {
     r.err = err
     res.json(r)
   });
 });
 
+
+router.get('/all', function(req, res, next) {
+  api.massSend('o3NPW0r_fi2BL8-zYrJuryidCuCY', 'Hello ' + ++ i, (err, r) => {
+    r.err = err
+    res.json(r)
+  });
+});
 
 module.exports = router;
