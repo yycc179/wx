4bf2c843f243b70680a74e693cfdb035f5b9098b
Yang Yang <yangyang@CDDEV01.cd.ad.availink.com>
2017-09-13 17:27:08 +0800

    add remote debug

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const WechatAPI = require('wechat-api')
 
-var api = new WechatAPI('wx752686772c3e8225', '1e050558f68abfb72675a0d01eca5e2a');
+var api = new WechatAPI('wx3b83bf8f1b698b05', 'a09d4a8e2553dfc2276903ce96108df2');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
